2d4f7ec0c29007b474591af6ab5c4a661ecc264d
MVPlauda <plaudadev@gmail.com>
2023-04-28 23:40:39 -0300

    Data&Hora

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime dataCorrente = DateTime.Now;
+        Console.WriteLine("Data/Hora: " + dataCorrente);
+    }
+
+}
+
